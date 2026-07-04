a21dcf608d61148a704e6390de19d3ac036dce3b
Sarvesh Dwivedi <2000locomail@gmail.com>
2026-07-04 11:06:57 +0530

    Add Simple Calculator function to perform addition of two numbers

---
+++ Simple_Calculator_Function/Simple_Calculator_Function.py
@@ -0,0 +1,7 @@
+def add_numbers(a,b):
+    return a + b
+if __name__ == '__main__':
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = add_numbers(num1, num2)
+    print(f"{num1} + {num2} = {result}")
